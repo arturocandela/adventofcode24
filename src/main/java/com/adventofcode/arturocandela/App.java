cfa637db6c3bcc2abac82116e12f9466f34e62ee
Arturo Candela <a.candelamolto@edu.gva.es>
2024-12-02 11:41:41 +0100

    Initial Commit

---
+++ src/main/java/com/adventofcode/arturocandela/App.java
@@ -0,0 +1,13 @@
+package com.adventofcode.arturocandela;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
